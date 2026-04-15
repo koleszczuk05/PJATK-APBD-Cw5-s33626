f243e178d018cbf805470502fb837dfbc8232db2
Krzysztof Oleszczuk <koleszczuk05@gmail.com>
2026-04-15 02:19:11 +0200

    Added 2 new classes to models Room.cs and Reservation.cs

---
+++ WebApplication1/Models/Reservation.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models;
+
+public class Reservation
+{
+    public int Id { get; set; }
+    public int RoomId { get; set; }
+    [Required]
+    public string OrganizerName { get; set; } = string.Empty;
+    [Required]
+    public string Topic { get; set; } = string.Empty;
+    public DateOnly Date { get; set; }
+    public TimeOnly StartTime { get; set; }
+    public TimeOnly EndTime { get; set; }
+    public string Status { get; set; } = string.Empty;
+    
+    
+}
