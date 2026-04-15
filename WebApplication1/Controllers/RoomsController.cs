8361634b7b228b9f6b4d79eef80a6cf11c68f6e6
Krzysztof Oleszczuk <koleszczuk05@gmail.com>
2026-04-16 00:33:26 +0200

    added RoomsController.cs with fully implemented methods and Database.cs with 2 static list that holds data

---
+++ WebApplication1/Controllers/RoomsController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class RoomsController : ControllerBase
+{
+
+    [HttpGet("{id:int}")]
+    public IActionResult GetRoom(int id)
+    {
+        var room = Database.Rooms.FirstOrDefault(x => x.Id == id);
+        if (room == null)
+        {
+            return NotFound($"Nie znaleziono sali o ID: {id}");
+        }
+        return Ok(room);
+    }
+    
+    [HttpGet("building/{buildingCode}")]
+    public IActionResult GetRoomsByBuilding(string buildingCode)
+    {
+        return Ok(Database.Rooms.Where(room => room.BuildingCode == buildingCode).ToList());
+    }
+
+    [HttpGet]
+    public IActionResult GetFiltered(int? minCapacity, bool? hasProjector, bool? activeOnly)
+    {
+        var rooms = Database.Rooms.AsEnumerable();
+        if (minCapacity.HasValue)
+        {
+            rooms = rooms.Where(room => room.Capacity >= minCapacity.Value);
+        }
+
+        if (hasProjector.HasValue)
+        {
+            rooms = rooms.Where(room => room.HasProjector == hasProjector.Value);
+        }
+
+        if (activeOnly.HasValue)
+        {
+            rooms = rooms.Where(room => room.IsActive == activeOnly.Value);
+        }
+        
+        return Ok(rooms.ToList());
+    }
+
+    [HttpPost]
+    public IActionResult CreateRoom([FromBody] Room room)
+    {
+        var maxId = Database.Rooms.Max(x => x.Id);
+        room.Id = maxId + 1;
+        Database.Rooms.Add(room);
+        return CreatedAtAction(nameof(GetRoom), new { id = maxId + 1 }, room);
+    }
+
+    [HttpPut("{id:int}")]
+    public IActionResult UpdateRoom(int id,[FromBody] Room room)
+    {
+        var targetRoom = Database.Rooms.FirstOrDefault(r => r.Id == id);
+
+        if (targetRoom == null)
+        {
+            return NotFound($"Nie znaleziono sali o ID: {id}");
+        }
+        
+        targetRoom.Name = room.Name;
+        targetRoom.BuildingCode = room.BuildingCode;
+        targetRoom.Floor =  room.Floor;
+        targetRoom.Capacity = room.Capacity;
+        targetRoom.HasProjector = room.HasProjector;
+        targetRoom.IsActive = room.IsActive;
+        
+        return NoContent();
+    }
+
+    [HttpDelete("{id:int}")]
+    public IActionResult DeleteRoom(int id)
+    {
+        var room = Database.Rooms.FirstOrDefault(x => x.Id == id);
+
+        if (room == null)
+        {
+            return NotFound($"Nie znaleziono sali o ID: {id}");
+        }
+        Database.Rooms.Remove(room);
+        return NoContent();
+    }
+    
+    
+    
+}
---
+++ WebApplication1/Data/Database.cs
@@ -0,0 +1,68 @@
+﻿using WebApplication1.Models;
+
+namespace WebApplication1.Data;
+
+public class Database
+{
+    public static List<Room> Rooms =
+    [
+        new Room
+        {
+            Id = 1, Name = "Lab 101", BuildingCode = "A", Floor = 1, Capacity = 30, HasProjector = true, IsActive = true
+        },
+        new Room
+        {
+            Id = 2, Name = "Lab 102", BuildingCode = "A", Floor = 1, Capacity = 15, HasProjector = false,
+            IsActive = true
+        },
+        new Room
+        {
+            Id = 3, Name = "Aula Główna", BuildingCode = "B", Floor = 0, Capacity = 150, HasProjector = true,
+            IsActive = true
+        },
+        new Room
+        {
+            Id = 4, Name = "Sala 204", BuildingCode = "C", Floor = 2, Capacity = 20, HasProjector = true,
+            IsActive = false
+        }, // Ta sala jest nieaktywna!
+        new Room
+        {
+            Id = 5, Name = "Pokój cichy", BuildingCode = "C", Floor = 3, Capacity = 4, HasProjector = false,
+            IsActive = true
+        }
+    ];
+
+    public static List<Reservation> Reservations =
+    [
+        new Reservation
+        {
+            Id = 1, RoomId = 1, OrganizerName = "Jan Kowalski", Topic = "Wprowadzenie do C#",
+            Date = new DateOnly(2026, 5, 10), StartTime = new TimeOnly(8, 0), EndTime = new TimeOnly(10, 0),
+            Status = "confirmed"
+        },
+        new Reservation
+        {
+            Id = 2, RoomId = 1, OrganizerName = "Anna Nowak", Topic = "Konsultacje projektowe",
+            Date = new DateOnly(2026, 5, 10), StartTime = new TimeOnly(10, 30), EndTime = new TimeOnly(12, 0),
+            Status = "planned"
+        },
+        new Reservation
+        {
+            Id = 3, RoomId = 3, OrganizerName = "Piotr Wiśniewski", Topic = "Wykład gościnny",
+            Date = new DateOnly(2026, 5, 12), StartTime = new TimeOnly(14, 0), EndTime = new TimeOnly(16, 0),
+            Status = "confirmed"
+        },
+        new Reservation
+        {
+            Id = 4, RoomId = 5, OrganizerName = "Kasia Lewandowska", Topic = "Praca w ciszy",
+            Date = new DateOnly(2026, 5, 12), StartTime = new TimeOnly(9, 0), EndTime = new TimeOnly(15, 0),
+            Status = "planned"
+        },
+        new Reservation
+        {
+            Id = 5, RoomId = 2, OrganizerName = "Marek Dąbrowski", Topic = "Warsztaty REST API",
+            Date = new DateOnly(2026, 5, 15), StartTime = new TimeOnly(16, 0), EndTime = new TimeOnly(18, 30),
+            Status = "cancelled"
+        }
+    ];
+}
